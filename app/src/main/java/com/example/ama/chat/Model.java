fc634388a5628b6f2e419a3f645b3b0456a33e68
Ahmed Bogatyrev <sarmak90@mail.ru>
2016-05-03 15:23:10 +0300

    Добавлен уровень Model

---
+++ app/src/main/java/com/example/ama/chat/Model.java
@@ -0,0 +1,15 @@
+package com.example.ama.chat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Ama on 03.05.2016.
+ */
+public class Model {
+    private List<String> listMessages = new ArrayList<>();
+
+    public List<String> getListMessages() {
+        return listMessages;
+    }
+}
